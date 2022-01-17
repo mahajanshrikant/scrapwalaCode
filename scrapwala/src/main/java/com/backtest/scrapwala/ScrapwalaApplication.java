32798c3ef054a627dc22ce3c6d54b904305cf1ca
shrikant mahajan <shrikantmahajan10@gmail.com>
2022-01-17 21:27:32 +0530

    first version

---
+++ scrapwala/src/main/java/com/backtest/scrapwala/ScrapwalaApplication.java
@@ -0,0 +1,13 @@
+package com.backtest.scrapwala;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrapwalaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrapwalaApplication.class, args);
+	}
+
+}
---
+++ scrapwala/src/test/java/com/backtest/scrapwala/ScrapwalaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backtest.scrapwala;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrapwalaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
